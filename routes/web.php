69e1acf5ca42268207bf015b2add16fd15561cdb
mostafaadawy <mostafa_adawy@ymail.com>
2022-03-14 07:56:30 +0200

    adding routes for users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PHPUnit\TextUI\XmlConfiguration\Group;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Users routes
+Route::group(
+    ['namespace' => 'Users', 'prefix' => 'users', 'as' => 'usesr.', 'middleware' => []],
+    function () {
+        Route::get('/', function (){return view('Users.home');});
+    }
+);
